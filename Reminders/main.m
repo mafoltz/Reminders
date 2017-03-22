375e05460f51403e0a4f124adefe49bb6bd6e8f5
Marcelo Foltz <marceloandrighettofoltz@Marcelos-MacBook-Pro.local>
2017-03-22 17:54:01 -0300

    Initial Commit

---
+++ Reminders/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Reminders
+//
+//  Created by Marcelo Andrighetto Foltz on 22/03/17.
+//  Copyright © 2017 Marcelo Andrighetto Foltz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
